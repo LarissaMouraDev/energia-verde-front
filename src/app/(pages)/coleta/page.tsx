98a6b48854259bd2edd240243ec9b7e12b3f6e54
gabrielperucci <gabrielperucci@icloud.com>
2024-11-22 23:28:19 -0300

    feat: implement voltar

--- src/app/(pages)/coleta/page.tsx
+++ src/app/(pages)/coleta/page.tsx
@@ -1,11 +1,17 @@
+'use client'
 import Agenda from '@/components/coleta/agendar'
 import Header from '@/components/header/page'
 import React from 'react'
 
 export default function Page() {
+    const handleInicio = () => {
+        window.location.href = "/recompensar"
+    }
+
+
   return (
     <>
-      <Header />
+      <Header text="Coleta" handleFunction={handleInicio} />
       <div className='flex flex-col gap-6 mt-8 items-center p-8'>
         <Agenda />
       </div>
